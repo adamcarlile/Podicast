dbe5b231705e064a7823188e073552c0178cbb63
Adam Carlile <adam@benchmedia.co.uk>
2010-08-05 21:59:02 +0100

    Added order_by clause

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,7 @@
 class Podcast < ActiveRecord::Base
     
+  default_scope :order => :created_at
+    
   has_attached_file :audio, 
     :path => ":rails_root/public/upload/:rails_env/podcast/:id/:filename",
     :url => "/upload/:rails_env/podcast/:id/:filename"
